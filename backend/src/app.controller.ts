0e2f9406b3c9a68ddd733627a76ecf76693a0042
Emerson Pereira <emerson.pereira@bolttech.io>
2022-02-28 22:01:07 +0000

    move login to auth controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,19 +1,9 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/guards/local-auth.guard';
-import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly authService: AuthService
-  ) {}
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
+  constructor() {}
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
