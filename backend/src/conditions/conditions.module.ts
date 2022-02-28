c1471bc8120ebf14aadb93eaf9f3bbf28c53aee9
Emerson Pereira <emerson.pereira@bolttech.io>
2022-02-28 21:38:05 +0000

    bind db and services to conditions module

--- backend/src/conditions/conditions.module.ts
+++ backend/src/conditions/conditions.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConditionsService } from './conditions.service';
 import { ConditionsController } from './conditions.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Condition, ConditionSchema } from './schemas/condition.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Condition.name, schema: ConditionSchema }])],
   providers: [ConditionsService],
-  controllers: [ConditionsController]
+  controllers: [ConditionsController],
+  exports: [ConditionsService]
 })
 export class ConditionsModule {}
