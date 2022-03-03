842434183e308195d720409f50bc397c0bb20bac
Emerson Pereira <emerson.pereira@bolttech.io>
2022-03-03 00:44:52 +0000

    implement login info on top bar

--- frontend/src/components/TopBar/index.tsx
+++ frontend/src/components/TopBar/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../App';
 import './Style.css';
 
 function TopBar() {
+  let auth = useAuth();
+  let navigate = useNavigate();
+
   return (
     <section className="TopBar">
       <nav className='TopBar-Menu'>
-        <span>John Doe</span>
-        <span> | </span>
-        <a href="#">Logout</a>
+        {!auth.user && <span>Hello, Doctor!</span>}
+
+        {auth.user && (
+          <>
+            <span>{auth.user.name}</span>
+            <span> | </span>
+            <button
+              onClick={() => {
+                auth.signout(() => navigate("/login"));
+              }}
+            >Logout</button>
+          </>
+        )}
       </nav>
     </section>
   );
