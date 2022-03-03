453bf8d22c4bf46fb2473abe95097cfda524f712
Emerson Pereira <emerson.pereira@bolttech.io>
2022-03-03 00:33:58 +0000

    add user data to login response

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -22,6 +22,8 @@ export class AuthService {
   async login(user: any) {
     const payload = { email: user.email, sub: user.userId };
     return {
+      email: user.email,
+      name: user.name,
       access_token: this.jwtService.sign(payload),
     };
   }
