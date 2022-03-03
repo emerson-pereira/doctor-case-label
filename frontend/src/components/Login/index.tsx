77aa389d4333a9599d0b8263bea96b6f02bce5dd
Emerson Pereira <emerson.pereira@bolttech.io>
2022-03-03 00:44:31 +0000

    implement login functionality to component

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../App';
 import './Style.css';
 
-function handleSubmit(event: React.FormEvent<HTMLFormElement>) { }
-
 function Login() {
+  let navigate = useNavigate();
+  let location: any = useLocation();
+  let auth = useAuth();
+
+  let from = location.state?.from?.pathname || "/";
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    let formData = new FormData(event.currentTarget);
+    let email = formData.get("email") as string;
+    let password = formData.get("password") as string;
+
+    const loginResponse = await requestLogin({
+      email,
+      password
+    });
+
+    if (loginResponse.access_token) {
+      auth.signin({ email: loginResponse.email, name: loginResponse.name, token: loginResponse.access_token }, () => {
+        navigate(from, { replace: true });
+      });
+    }
+  }
+
   return (
     <section className="Login">
       <h2>Login</h2>
@@ -25,6 +50,17 @@ function Login() {
       </form>
     </section>
   );
+}
+
+async function requestLogin(credentials: any) {
+  const response = await fetch('http://localhost:4000/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials)
+  })
+  return response.json();
 }
 
 export default Login;
