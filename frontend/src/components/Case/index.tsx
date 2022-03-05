d055b8ae03ae1ee8cd147369f4a4d2ba750aa4ea
Emerson Pereira <emerson.pereira@bolttech.io>
2022-03-05 04:39:12 +0000

    implement submit review + next case

--- frontend/src/components/Case/index.tsx
+++ frontend/src/components/Case/index.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAuth } from '../../contexts/auth';
+import { useLocalState } from '../../contexts/state';
 import './Style.css';
 
+export async function getNextCase(token: string) {
+  const nextCase = await fetch('http://localhost:4000/cases/next', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  const string = await nextCase.text();
+  const json = string === "" ? {} : JSON.parse(string);
+  return json;
+}
+
 function Case() {
+  const { case: currentCase, updateCase } = useLocalState();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    async function populateConditions() {
+      const nextCase = await getNextCase(user.token);
+
+      if (!Object.keys(nextCase).length) {
+        alert('All finished. Enjoy the day!');
+        return;
+      }
+
+      updateCase({ caseId: nextCase._id, content: nextCase.content }, () => { });
+    }
+    populateConditions();
+  }, []);
+
   return (
     <section className="Case">
       <h2>Case</h2>
 
       <div className='Case-Content'>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, quia? Rem quibusdam amet dolor sequi deserunt eveniet veritatis rerum ea sunt quam corrupti sed, perferendis neque delectus, et adipisci nostrum.
+          {currentCase.content}
         </p>
       </div>
     </section>
--- frontend/src/components/MainSection/index.tsx
+++ frontend/src/components/MainSection/index.tsx
@@ -1,16 +1,59 @@
 import React from 'react';
-import Case from '../Case';
+import { useAuth } from '../../contexts/auth';
+import { useLocalState } from '../../contexts/state';
+import Case, { getNextCase } from '../Case';
 import Conditions from '../Conditions';
 import './Style.css';
 
+async function submitCurrentCase(caseId: string, conditionId: string, token: string) {
+  const caseResult = await fetch(`http://localhost:4000/cases/review/${caseId}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ conditionId })
+  });
+  return caseResult.json();
+}
+
 function MainSection() {
+  const { case: currentCase, selectedConditionId, updateCase } = useLocalState();
+  const { user } = useAuth();
+
+  async function handleNextCase() {
+    if (!selectedConditionId) {
+      alert('Oops, forgetting something?');
+      return;
+    }
+
+    const caseSubmitResult = await submitCurrentCase(
+      currentCase.caseId,
+      selectedConditionId,
+      user.token,
+    );
+
+    if (!caseSubmitResult.isReviewed) {
+      throw Error('Error on subimiting case to review.');
+    }
+
+    const nextCase = await getNextCase(user.token);
+
+    if (!Object.keys(nextCase).length) {
+      alert('All finished. Enjoy the day!');
+      return;
+    }
+
+    updateCase({ caseId: nextCase._id, content: nextCase.content }, () => { });
+  }
+
   return (
     <main className="MainSection">
       <Case />
       <aside className='MainSection-Aside'>
         <Conditions />
         <section className='MainSection-NextButton'>
-          <button>Next Case</button>
+          <button onClick={handleNextCase}>Next Case</button>
         </section>
       </aside>
     </main>
